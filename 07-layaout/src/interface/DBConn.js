31aa31c7b99b321d2b17d3acfb5cc1562b06a936
MartinCiro <52991306+MartinCiro@users.noreply.github.com>
2024-05-16 20:18:42 -0500

    add: se añade back

---
+++ 07-layaout/src/interface/DBConn.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+const config = require("../config.js");
+
+const dbauth = {
+    user: config.UserDB,
+    password: config.PasswordBD,
+    host: config.ServerDB,
+    port: config.PortDB,
+    database: config.Database,
+    ssl: { rejectUnauthorized: true }
+};
+/**
+ * Método para conectarse a la base de datos de nexia automation
+ * @returns {Promise<Pool>}
+ */
+function getConnection() {
+  return new Pool(dbauth);
+}
+
+module.exports = { getConnection };
