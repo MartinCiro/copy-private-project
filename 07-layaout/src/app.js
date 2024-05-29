e294c250dafc9c2957733398fd851a2f21e8c4a9
MartinCiro <52991306+MartinCiro@users.noreply.github.com>
2024-05-29 16:22:27 -0500

    se hace pruebas de servicio sin liveserver

--- 07-layaout/src/app.js
+++ 07-layaout/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const path = require('path');
 const config = require('./config.js');
 const routes = require('./routes.js');
 
@@ -15,9 +16,15 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '25mb' }));
 
+// Servir archivos estáticos desde la carpeta 'front'
+app.use(express.static(path.join(__dirname, '../front')));
+
+// Ruta para servir el archivo HTML principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../front/index.html'));
+});
+
 // Routes
 app.use(routes);
-
-
 
 module.exports = app;
